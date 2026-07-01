1d16b9350a7ab4f3754b41ac0e91a0e312388f94
Sarvani151 <sinagareddysarvani@gmail.com>
2026-07-01 18:00:03 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,78 @@
+Python 3.14.4 (v3.14.4:23116f998f6, Apr  7 2026, 09:45:22) [Clang 17.0.0 (clang-1700.6.4.2)] on darwin
+Enter "help" below or click "Help" above for more information.
+a="vijayawada"
+a[0]
+'v'
+a[4]
+'y'
+a[10]
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    a[10]
+IndexError: string index out of range
+>>> a[9]
+'a'
+>>> a[0]+a[1]+a[2]
+'vij'
+>>> a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+'vijaya'
+>>> a="I am in class"
+>>> a[8]
+'c'
+>>> a[5]
+'i'
+>>> a[3]
+'m'
+>>> a[1]
+' '
+>>> a[0]
+'I'
+>>> a[2]+a[3]
+'am'
+>>> a[5]+a[6]
+'in'
+>>> a[8]+a[9]+a[10]
+'cla'
+>>> a="simple is better than complex"
+>>> a[11]+a[12]+a[13]+a[14]+a[15]+a[16]
+'etter '
+>>> a[10]+a[11]+a[12]+a[13]+a[14]+a[15]+a[16]
+'better '
+>>> a[22]+a[23]+a[24]+a[25]+a[26]+a[27]
+'comple'
+>>> a[22]+a[23]+a[24]+a[25]+a[26]+a[27]+a[28]
+'complex'
+>>> a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+'simple'
+>>> b=codegnan it solutions
+SyntaxError: invalid syntax
+>>> b=codegnan
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    b=codegnan
+NameError: name 'codegnan' is not defined
+>>> b="codegnan it solutions"
+>>> b[12]+b[13]+b[14]+b[15]+b[16]+b[17]+b[18]+b[19]+b[20]
+'solutions'
+>>> b[9]+b[10]
+'it'
+>>> b[0]+b[1]+b[2]+b[3]+b[4]+b[5]+b[6]+b[7]
+'codegnan'
+>>> a="i am learning python"
+>>> a[-6]+a[-5]+a[-4]+a[-3]+a[-2]+a[-1]
+'python'
+>>> a[-15]+a[-14]+a[-13]+a[-12]+a[-11]
+'learn'
+>>> a[-18]+a[-17]
+'am'
+>>> a="python full satck"
+>>> a[-5]+a[-4]+a[-3]+a[-2]+a[-1]
+'satck'
+>>> a[-9]+a[-8]+a[-7]+a[-6]
+'ull '
+>>> a[-10]+a[-9]+a[-8]+a[-7]
+'full'
+>>> a[-17]+a[-16]+a[-15]+a[-14]+a[-13]
+'pytho'
+>>> a[-17]+a[-16]+a[-15]+a[-14]+a[-13]+a[-12]
+'python'
