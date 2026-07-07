a4678eaaec555894ceed8b956717eeafcaa4ba1a
Sarvani151 <sinagareddysarvani@gmail.com>
2026-07-07 19:42:07 +0530

    Add files via upload

---
+++ addition on runtime.py
@@ -0,0 +1,73 @@
+'''a=2
+b=4
+print(a+b)'''
+
+'''a=6
+b=8
+c=a+b
+print(c)'''
+
+#print(10+10)
+
+#run_time input()
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+'''a=str(input("data1"))
+b=str(input("data2"))
+print(a+b)'''
+
+
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+
+'''fname=input("first name")
+lname=input("last name")
+print((fname+" "+lname).title())'''
+
+
+'''a=complex(input("enter the a value"))
+b=complex(input("enter the b value"))
+print(a+b)'''
+
+
+
+'''a=bool(input("enter the a value"))
+b=bool(input("enter the b value"))
+c=bool(input("enter the c value"))
+print(a+b+c)'''
+
+'''a=int(input("a value"
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=input(choose the option
+                 1 add
+                 2 sub
+                 3 muls)s
+print(a+b)
+print(a-b)
+print(a*b)'''
+      
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=input("choose the option add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+a=int(input())
+b=int(input())
+print(a+b)
