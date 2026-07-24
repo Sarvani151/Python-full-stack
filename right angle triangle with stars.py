d951a4643e47b5c9dfb2962573fe9ba41b0109d0
Sarvani151 <sinagareddysarvani@gmail.com>
2026-07-24 12:18:07 +0530

    Add files via upload

---
+++ right angle triangle with stars.py
@@ -0,0 +1,19 @@
+'''n=int(input("Enter number of stars:"))
+for i in range(n):
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print()'''
+
+'''n=int(input("Enter number of stars:"))
+for i in range(n):
+    for j in range(n-i):
+        print("*",end=" ")
+    print()'''
+'''n=int(input("Enter number of stars:"))
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()'''
+
+
+
