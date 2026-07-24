cc2711cf5200044307698540dd005c91e7b17165
Sarvani151 <sinagareddysarvani@gmail.com>
2026-07-24 12:13:09 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,83 @@
+#loops
+#for,while,range,break,continue,pass
+#for loop()
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=(10,20,30,40,50)
+for i in a:
+    print(i)
+print(type(a))
+print(type(i'''
+
+'''a={10,20,30,40,50}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+ 
+'''d={"year":2026,"month":"july","date":10}
+for i in d:
+    print(i)
+for i in d.keys():
+    print(i)
+for i in d.values():
+    print(i)
+for i in d.items():
+    print(i)
+print(type(d))
+print(type(i))'''
+
+'''a="codegnan"
+for i in a:
+   print(i)'''
+
+'''a=[3.4,5.6]
+for i in a:
+    print(i)'''
+'''a=[True,False]
+for i in a:
+    print(i)'''
+
+'''a=["apple","banana","mango"]
+b=str(a)
+print(b.upper())'''
+
+'''a=["apple","banana","mango"]
+b=[]
+for i in a:
+    b.append(i.upper())
+print(b)'''
+
+
+'''a=[10,20,30,40,50,"code"]
+a.extend(["c","o","d","e"])
+print(a)'''
+
+'''a=[10,20,30,40,50,"code"]
+a.extend(["c","o","d","e"])
+print(a)'''
+
+'''b=(5,6,7,8,9,10)
+c=b[:-1]
+print(c)'''
+
+'''e=[7,9,2,0,1,4,9,3,20]
+e.sort()
+print(e)'''
+
+
+
+
+
